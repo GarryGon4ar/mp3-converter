7b7046b4596d2e9b7b52918a8f571b7f3d28fd28
Dzhumabaev Erlan <garrygon4ar@gmail.com>
2019-05-28 06:04:46 +0600

    delete comments, put context variable in render, delete unicode method

--- main/models.py
+++ main/models.py
@@ -1,26 +1,9 @@
 from django.db import models
 
 
-# class AbstractModel(models.Model):
-#     created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True, null=True)
-#     updated_at = models.DateTimeField(verbose_name='дата обновления', auto_now=True, null=True)
-#
-#     def __unicode__(self):
-#         pass
-#
-#     def __str__(self):
-#         return self.__unicode__()
-#
-#     class Meta:
-#         abstract = True
-
 class Song(models.Model):
     link = models.URLField(max_length=250)
     created_at = models.DateTimeField(verbose_name='request sent', auto_now_add=True, null=True)
 
-    def __unicode__(self):
-        return self.link
-
     def __str__(self):
-        return self.__unicode__()
-
+        return self.link
--- main/views.py
+++ main/views.py
@@ -1,55 +1,13 @@
 from django.shortcuts import render, redirect
 import youtube_dl
-# from django.urls import reverse_lazy
-# from django.views.generic import FormView
-# from .models import Song
 from main.models import Song
 from .forms import DownloadForm
 
-# class DownloadView(FormView):
-#     form_class = DownloadForm
-#     model = Song
-#     template_name = 'index.html'
-#     success_url = reverse_lazy('index')
-#
-#     def form_valid(self, form):
-#         link = form.cleaned_data.get('url')
-#         ydl_opts = {
-#             'format': 'bestaudio/best',
-#             'postprocessors': [{
-#                 'key': 'FFmpegExtractAudio',
-#                 'preferredcodec': 'mp3',
-#                 'preferredquality': '192',
-#             }]}
-#         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#             ydl.download([link])
-#         return super(DownloadView, self).form_valid(form)
-#
-#     def post(self, request, *args, **kwargs):
-#         """
-#         Handle POST requests: instantiate a form instance with the passed
-#         POST variables and then check if it's valid.
-#         """
-#         form = self.get_form()
-#         link = form.get('url')
-#         ydl_opts = {
-#             'format': 'bestaudio/best',
-#             'postprocessors': [{
-#                 'key': 'FFmpegExtractAudio',
-#                 'preferredcodec': 'mp3',
-#                 'preferredquality': '192',
-#             }]}
-#         if form.is_valid():
-#             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#                 ydl.download([link])
-#             return self.form_valid(form)
-#         else:
-#             return self.form_invalid(form)
 
 def download(request):
     form = DownloadForm()
     if request.method == "POST":
-        form = DownloadForm(request.POST) #if no files
+        form = DownloadForm(request.POST)
         if form.is_valid():
             video_url = form.cleaned_data.get('url')
             Song.objects.create(link=video_url)
@@ -65,8 +23,7 @@ def download(request):
                 info = ydl.extract_info(video_url, download=False)
 
                 return redirect(info['formats'][0]['url'])
-    context = {
-        'form': form,
-    }
 
-    return render(request, "index.html", context)
+    return render(request, "index.html", context={
+        'form': form,
+    })
